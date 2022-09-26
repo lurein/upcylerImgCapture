8e9d1544b70e73a7f889c1c8e45dc24a87dbd8c0
Lurein Perera <lureinperera@gmail.com>
2022-09-25 23:40:07 -0400

    next commit

--- src/views/PhotoCaptureView.vue
+++ src/views/PhotoCaptureView.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="about">
       <h1>Capture an image of your t-shirt</h1>
+      <h1>{{ url }}</h1>
       <camera :resolution="{ width: 375, height: 812 }" autoplay></camera>
+      <button @click="snapshot">Create snapshot</button>
     </div>
   </template>
   
@@ -15,16 +17,42 @@
   }
   </style>
 
-  <script>
+  <script lang="ts">
     import Camera from "simple-vue-camera";
+    import { ref, defineComponent } from 'vue';
 
     //inside your vue options api
-    export default {
-        components: {
+    export default defineComponent({
+      data() {
+        return {
+          blobUrl: '',
+        }
+      },
+      components: {
           Camera,
+        },
+        setup() {
+        // Get a reference of the component
+  
+        const camera = ref<InstanceType<typeof Camera>>();
+        
+        var url = ""
+        // Use camera reference to call functions
+        const snapshot = async () => {
+            const blob = await camera.value?.snapshot() as Blob;
+
+            // To show the screenshot with an image tag, create a url
+            url = URL.createObjectURL(blob);
+        }
+
+        return {
+            camera,
+            snapshot,
+            url
+        }
         },
         mounted() {
           console.log('mounted')
         }
-    }
+    });
   </script>
